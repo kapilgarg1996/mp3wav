f0159adf6aff6866b88b726fc998c670fb6d2120
kapil garg <kapilgarg1996@gmail.com>
2016-01-21 15:24:47 +0530

    Fixed argument problem

--- mp3wav/exceptions/overwriteexception.py
+++ mp3wav/exceptions/overwriteexception.py
@@ -1,7 +1,7 @@
 from errordialog import ErrorDialog
 
 class OverWriteException(Exception):
-	def __init__(self, parent):
+	def __init__(self, parent=None):
 		super(OverWriteException, self).__init__()
 		errorMessage = "The Destination File Already Exists"+\
 			"\nTo overwrite it, tick the box in the application"
